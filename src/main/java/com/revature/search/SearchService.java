c025530d73a0f6d3961c92758d7cf8ff04744f62
Alexander Snell <alexksnell@gmail.com>
2022-01-07 21:37:26 -0500

    [MISC] Adjusted log statement format

--- src/main/java/com/revature/search/SearchService.java
+++ src/main/java/com/revature/search/SearchService.java
@@ -2,13 +2,10 @@ package com.revature.search;
 
 import com.revature.users.UserRepository;
 import com.revature.search.dtos.SearchResponse;
-import io.micrometer.core.instrument.search.Search;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import javax.annotation.PostConstruct;
 
 @Service
 public class SearchService {
@@ -26,10 +23,10 @@ public class SearchService {
 
     public SearchResponse queryUsers(String query) {
         if (searchCache.contains(query)) {
-            logger.info("Returning cached search results for query: {}", query);
+            logger.info("Cache HIT for valid bucket containing SearchResponse for query: {}", query);
             return searchCache.get(query);
         }
-        logger.info("Cache miss for users matching query: {}", query);
+        logger.info("Cache MISS for valid bucket containing SearchResponse for query: {}", query);
         SearchResponse response = new SearchResponse(userRepository.findUsersByEmailContains(query));
         searchCache.put(query, response);
         return searchCache.get(query);
