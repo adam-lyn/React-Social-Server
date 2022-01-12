c54c59399b311149bfb2931ea357c4e9648b2606
Heather <ada@VIIchariot.com>
2022-01-11 18:15:25 -0600

    Successful Controller unit test

--- src/test/java/com/revature/search/TestSearchControllerIntegration.java
+++ src/test/java/com/revature/search/TestSearchControllerIntegration.java
@@ -1,15 +1,21 @@
 package com.revature.search;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Test;
+import com.revature.ReverbApplication;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(classes = ReverbApplication.class)
+@ActiveProfiles("test")
 public class TestSearchControllerIntegration {
 
     private MockMvc mockMvc;
@@ -23,12 +29,15 @@ public class TestSearchControllerIntegration {
     }
 
     @BeforeEach
-    public void setup() {
+    public void setup() throws Exception {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
     }
 
     @Test
-    public void test_queryByString_() {
-
+    public void test_queryByString_returns200_givenStringInput() throws Exception {
+        mockMvc.perform(get("/api/search?query=test"))
+               .andDo(print())
+               .andExpect(status().is(200))
+               .andReturn();
     }
 }
