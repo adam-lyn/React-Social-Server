f4477c288dfa54aee21fff01d830548ad412330f
Adam Lyn <adam273@revature.net>
2022-04-11 15:53:30 -0400

    Revert "created Notification and NotificationType models in preparation for setting up and implementing notification logic."
    This reverts commit 488b2ff278505b9410070d540288d19d2dfdcd7b.

--- src/main/java/com/revature/exceptions/NotificationNotFoundExeption.java
+++
@@ -1,8 +0,0 @@
-package com.revature.exceptions;
-
-public class NotificationNotFoundExeption extends RuntimeException {
-    @Override
-    public String getMessage() {
-        return "Could not find notification!";
-    }
-}
--- src/main/java/com/revature/notifications/NotificationController.java
+++
@@ -1,4 +0,0 @@
-package com.revature.notifications;
-
-public class NotificationController {
-}
